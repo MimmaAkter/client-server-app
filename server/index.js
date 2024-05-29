04f5f644c6ace97004343742fccb20e0df06121a
mimma-akter <developermernstack436@gmail.com>
2024-05-29 23:36:36 +0600

    cloudinary crud

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-app.use('/',(req,res)=>{
-    res.send("server is running");
-});
+dotenv.config();
 
-app.listen(5000,console.log("server started on PORT 5000"))
+// Connect DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("mongoDB is connected"))
+  .catch((err) => console.log(err));
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Route
+app.use("/user", require("./routes/user"));
+
+app.listen(process.env.PORT, () => console.log("Server is running"));
